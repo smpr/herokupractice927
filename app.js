80678d63949b26cc96d8f03b8002457f276356f4
smpr <gcbrown1116@gmail.com>
2017-09-27 10:48:51 -0400

    Added mongo db

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI); 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+ const db = mongoose.connection
+ db.on('error', (error)=>{
+   console.log(error)
+ })
+ db.once('open', ()=>{
+   console.log("Conected to mongodb")
+ })
 var app = express();
 
 // view engine setup
