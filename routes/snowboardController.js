eec55331e5e01f9bb090afb80cd9c865aaa56b95
smpr <gcbrown1116@gmail.com>
2017-09-28 09:13:48 -0400

    It works! somehow.....

--- routes/snowboardController.js
+++ routes/snowboardController.js
@@ -4,18 +4,13 @@ const router = express.Router({ mergeParams: true })
 const Schema = require("../db/schema.js");
 const CompanyModel = Schema.CompanyModel;
 
-// INDEX route
+// INDEX
 router.get('/', (request, response) => {
 
-    // GRAB the company ID from the parameters
     const companyId = request.params.companyId
 
-    // Use the CompanyModel to find the company by ID
     CompanyModel.findById(companyId)
         .then((company) => {
-            // THEN once you have found the company in the database
-            // RENDER the company and its EMBEDDED snowboard info 
-            // using Handlebars
             response.render('snowboards/index', {
                 company: company
             })
@@ -29,12 +24,8 @@ router.get('/', (request, response) => {
 // NEW route
 router.get('/new', (request, response) => {
 
-    // GRAB the company ID from the parameters
     const companyId = request.params.companyId
 
-    // RENDER a new form for a fresh Snowboard,
-    // also passing the companyId to use in the
-    // form's ACTION
     response.render('snowboards/new', {
         companyId: companyId
     })
@@ -43,26 +34,15 @@ router.get('/new', (request, response) => {
 // CREATE route
 router.post('/', (request, response) => {
 
-    // GRAB the company ID from the parameters
     const companyId = request.params.companyId
-
-    // GRAB the new snowboard info from the request body
     const newSnowboard = request.body
 
-    // USE the CompanyModel to find the company by ID
     CompanyModel.findById(companyId)
         .then((company) => {
-            // THEN once you have found the company from the database
-            // PUSH the new snowboard object into the Company's 
-            // snowboard array            
             company.snowboards.push(newSnowboard)
-
-            // SAVE the company and return the PROMISE
             return company.save()
         })
         .then((company) => {
-            // THEN once the company has been saved, 
-            // REDIRECT to the Snowboards index for that company
             response.redirect(`/companies/${companyId}/snowboards`)
         })
 
@@ -71,21 +51,13 @@ router.post('/', (request, response) => {
 // EDIT route
 router.get('/:snowboardId/edit', (request, response) => {
 
-    // GRAB the company ID from the parameters
     const companyId = request.params.companyId
-
-    // GRAB the snowboard ID from the parameters
     const snowboardId = request.params.snowboardId
-
-    // USE the CompanyModel to find the company by ID
+    
     CompanyModel.findById(companyId)
         .then((company) => {
-            // THEN once the company has been returned,
-            // FIND the snowboard by ID that you want to edit
             const snowboard = company.snowboards.id(snowboardId)
 
-            // RENDER a form pre-populated with that snowboard info,
-            // ALSO passing the companyId to use for the form's ACTION
             response.render('snowboards/edit', {
                 snowboard: snowboard,
                 companyId: companyId
@@ -99,33 +71,20 @@ router.get('/:snowboardId/edit', (request, response) => {
 // UPDATE route
 router.put('/:snowboardId', (request, response) => {
 
-    // GRAB the company ID from the parameters
     const companyId = request.params.companyId
-
-    // GRAB the snowboard ID from the parameters
     const snowboardId = request.params.snowboardId
-
-    // GRAB the updated snowboard object from the request body
     const updatedSnowboard = request.body
-
-    // USE the CompanyModel to find the company by ID
+    
     CompanyModel.findById(companyId)
         .then((company) => {
-            // THEN once the company has been returned,
-            // FIND the snowboard by ID from the company's snowboards
             const snowboard = company.snowboards.id(snowboardId)
 
-            // MAP each attribute from the updated snowboard object to
-            // the same attribute on the original snowboard
             snowboard.name = updatedSnowboard.name
             snowboard.price = updatedSnowboard.price
 
-            // SAVE the updated company and return the PROMISE
             return company.save()
         })
         .then(() => {
-            // THEN once the company has saved, REDIRECT to the 
-            // snowboard's SHOW page
             response.redirect(`/companies/${companyId}/snowboards/${snowboardId}`)
         })
 
@@ -134,21 +93,13 @@ router.put('/:snowboardId', (request, response) => {
 // SHOW route
 router.get('/:snowboardId', (request, response) => {
 
-    // GRAB the company ID from the parameters
     const companyId = request.params.companyId
-    
-    // GRAB the snowboard ID from the parameters
     const snowboardId = request.params.snowboardId
 
-    // USE the CompanyModel to find the company by ID
     CompanyModel.findById(companyId)
         .then((company) => {
-            // THEN once the company has been returned,
-            // FIND the snowboard by ID from the company's snowboards
             const snowboard = company.snowboards.id(snowboardId)
 
-            // THEN render the snowboard info using Handlebars
-            // and pass the companyId to use in link URLs
             response.render('snowboards/show', {
                 snowboard: snowboard,
                 companyId: companyId
@@ -162,25 +113,16 @@ router.get('/:snowboardId', (request, response) => {
 // DELETE route
 router.get('/:snowboardId/delete', (request, response) => {
 
-    // GRAB the company ID from the parameters
     const companyId = request.params.companyId
-    
-    // GRAB the snowboard ID from the parameters
     const snowboardId = request.params.snowboardId
 
-    // USE the CompanyModel to find the company by ID
     CompanyModel.findById(companyId)
         .then((company) => {
-            // THEN once the company has been returned,
-            // REMOVE the snowboard from the company's snowboard array
             const snowboard = company.snowboards.id(snowboardId).remove()
 
-            // THEN save the company and return the PROMISE
             return company.save()
         })
         .then(() => {
-            // THEN once the company has saved, redirect to the 
-            // company's Snowboards INDEX page
             response.redirect(`/companies/${companyId}/snowboards`)
         })
 })
